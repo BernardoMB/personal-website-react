f8ef7383be7822ef959505e0d5df244a82aa0224
BernardoMB <bmondragonbrozon@gmail.com>
2024-05-04 22:25:38 -0400

    Added code to render dynamic content in the ui

--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -6,18 +6,27 @@ import {
     TabButton5
 } from "../TabButton/TabButton"
 import { useState } from "react" // Import the useState hook
+import testimonies from "./tertimonies.json"
 
 export function Tabs() {
     //const stateArray = useState('Select a tab')
     // Or use array destructuring
     const [ selectedTab, setSelectedTab ] = useState('Select a tab')
+    const [ testimony, setTestimony ] = useState({
+        testimonyText: testimonies[0].testimony,
+        testimonyAuthor: testimonies[0].author
+    })
     function handleSelect() {
         console.log('Handle select')
     }
     function handleWorkingTabSelected(selectedTab) {
         console.log('Handle working tab selected', selectedTab)
-        const tabContent = `${selectedTab} selected`
+        const tabContent = `${selectedTab + 5} selected`
         setSelectedTab(tabContent)
+        setTestimony({
+            testimonyText: testimonies[selectedTab].testimony,
+            testimonyAuthor: testimonies[selectedTab].author
+        })
     }
     return (
         <>
@@ -26,12 +35,19 @@ export function Tabs() {
                 <TabButton2>Tab2</TabButton2>
                 <TabButton3>Tab3</TabButton3>
                 <TabButton4 onClick={handleSelect}>Tab4</TabButton4>
-                <TabButton5 onSelect={() => handleWorkingTabSelected('Tab5')}>Tab5 (working)</TabButton5>
-                <TabButton5 onSelect={() => handleWorkingTabSelected('Tab6')}>Tab6 (working)</TabButton5>
-                <TabButton5 onSelect={() => handleWorkingTabSelected('Tab7')}>Tab7 (working)</TabButton5>
-                <TabButton5 onSelect={() => handleWorkingTabSelected('Tab8')}>Tab8 (working)</TabButton5>
+                <TabButton5 onSelect={() => handleWorkingTabSelected(0)}>Tab5 (working)</TabButton5>
+                <TabButton5 onSelect={() => handleWorkingTabSelected(1)}>Tab6 (working)</TabButton5>
+                <TabButton5 onSelect={() => handleWorkingTabSelected(2)}>Tab7 (working)</TabButton5>
+                <TabButton5 onSelect={() => handleWorkingTabSelected(3)}>Tab8 (working)</TabButton5>
+                <TabButton5 onSelect={() => handleWorkingTabSelected(4)}>Tab9 (working)</TabButton5>
             </menu>
             {selectedTab}
+            <div className="testimony">
+                <div className="testimony-container">
+                    <div className="testimony-text">{testimony.testimonyText}</div>
+                    <div className="testimony-author">{testimony.testimonyAuthor}</div>
+                </div>
+            </div>
         </>
     )
 }
