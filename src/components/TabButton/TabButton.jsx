03fa7eb5597197f511cf0b7459cf268be9f06c49
BernardoMB <bmondragonbrozon@gmail.com>
2024-05-04 21:18:31 -0400

    Added component using the children prop

--- src/components/TabButton/TabButton.jsx
+++ src/components/TabButton/TabButton.jsx
@@ -1,4 +1,18 @@
-export function TabButton({children}) {
+export function TabButton(props) {
+    return (
+        <li>
+            <button>{props.children}</button>
+        </li>
+    )
+}
+
+/**
+ * Children is being pulled out with object destructuring.
+ * Children are the elements that are inside the component html tags
+ * @param {*} param0 
+ * @returns 
+ */
+export function TabButton2({children}) {
     return (
         <li>
             <button>{children}</button>
--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -1,10 +1,10 @@
-import { TabButton } from "../TabButton/TabButton"
+import { TabButton, TabButton2 } from "../TabButton/TabButton"
 
 export function Tabs() {
     return (
         <menu>
             <TabButton>Tab1</TabButton>
-            <TabButton>Tab2</TabButton>
+            <TabButton2>Tab2</TabButton2>
             <TabButton>Tab3</TabButton>
         </menu>
     )
